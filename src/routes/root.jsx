92e031b678f9f4235ef8aef5b4221b20329d1bc3
Toxe <toxe@toxe.net>
2025-04-14 15:51:26 +0200

    Replace current history element when typing in search field

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -30,7 +30,12 @@ export default function Root() {
                 <h1>React Router Contacts</h1>
                 <div>
                     <Form id="search-form" role="search">
-                        <input id="q" className={searching ? "loading" : ""} aria-label="Search contacts" placeholder="Search" type="search" name="q" defaultValue={q} onChange={(event) => submit(event.currentTarget.form)} />
+                        <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" defaultValue={q}
+                            className={searching ? "loading" : ""}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, { replace: !isFirstSearch });
+                            }} />
                         <div id="search-spinner" aria-hidden hidden={!searching} />
                         <div className="sr-only" aria-live="polite" ></div>
                     </Form>
