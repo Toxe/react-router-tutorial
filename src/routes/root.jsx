2cd08be37acc4dfe6cd9c5360533bcadd08f124f
Toxe <toxe@toxe.net>
2025-04-14 13:45:51 +0200

    Highlight active contact in navigation

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, Form, useLoaderData, redirect } from "react-router-dom";
+import { Outlet, NavLink, Form, useLoaderData, redirect } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
@@ -33,13 +33,13 @@ export default function Root() {
                         <ul>
                             {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+                                    <NavLink to={`contacts/${contact.id}`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
                                         {contact.first || contact.last ? (
                                             <>{contact.first} {contact.last}</>
                                         ) : (
                                             <i>No Name</i>
                                         )}{" "} {contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
